80479ecd111b3c23e4db61c4fdca27dac7757b8e
kamalrajp@gmail.com <kamalrajp@gmail.com>
2018-12-16 09:13:26 +0800

    components

--- router-demo/src/app/app-routing.module.ts
+++ router-demo/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { DepartmentListComponent } from './department-list/department-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'employees', component: EmployeeListComponent},
+  { path: 'departments', component: DepartmentListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ router-demo/src/app/department-list/department-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-department-list',
+  templateUrl: './department-list.component.html',
+  styles: []
+})
+export class DepartmentListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
